8b70f0405644d2328cffbf55c16e0475cf97632d
vlad <vlad.gusiev@triogeeks.com>
2020-10-11 18:27:43 +0300

    heroku +

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("/api/config/paypal", (req, res) => {
 app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+    res.sendFile(path.join(`${__dirname}/frontend/build/index.html`));
 });
 
 app.listen(config.PORT, () => {
