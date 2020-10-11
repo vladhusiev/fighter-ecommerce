2cf3ae06c101008f251fa814e81c25e7e416e4b9
vlad <vlad.gusiev@triogeeks.com>
2020-10-11 18:41:44 +0300

    +-

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use("/api/orders", orderRoute);
 app.get("/api/config/paypal", (req, res) => {
     res.send(config.PAYPAL_CLIENT_ID);
 });
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
+app.use(express.static(path.join(__dirname, "/../frontend/build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(`${__dirname}/frontend/build/index.html`));
+    res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
 app.listen(config.PORT, () => {
