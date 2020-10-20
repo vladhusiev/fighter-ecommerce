8d8d885f52610921492e4037c0ce89f018e17503
vlad <vlad.gusiev@triogeeks.com>
2020-10-20 17:12:10 +0300

    added /

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import multer from 'multer';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
-import config from '../config';
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, '/uploads/');
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.jpg`);
