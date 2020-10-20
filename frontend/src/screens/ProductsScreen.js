df27ece61161081d5eff15bf9147a22f101572ba
vlad <vlad.gusiev@triogeeks.com>
2020-10-20 19:55:55 +0300

    fixedFinal

--- frontend/src/screens/ProductsScreen.js
+++ frontend/src/screens/ProductsScreen.js
@@ -127,7 +127,7 @@ function ProductsScreen(props) {
       .then((response) => {
         console.log(response.data)
         Object.values(response.data).map((item, index) => {
-          imagesArr.push(Object.values(item)[2]);
+          imagesArr.push(Object.values(item)[13]);
         })
         setImage(imagesArr);
         setUploading(false);
